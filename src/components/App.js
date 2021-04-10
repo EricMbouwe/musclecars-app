4055f6acb96cd8dd00285a17b198de73d5ce1318
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-10 22:46:24 +0100

    add router to app

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import { Route, Router, Switch } from 'react-router';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Switch>
+          <Route exact path="/" component={} />
+          <Route exact path="/" component={} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
