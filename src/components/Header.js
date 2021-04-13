8a58befb8398b10721b978a91731725b6709dcb3
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-13 16:03:36 +0100

    update header current user link

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,7 +10,7 @@ const Header = ({ dispatch, currentUser, loggedIn }) => (
         <NavLink to="/login">Login</NavLink>
         <NavLink to="/signup">Sign up</NavLink>
         {loggedIn && <h2>{currentUser.name}</h2>}
-        {loggedIn && <Link to={`users/${currentUser.id}/appointments`}>My appointments</Link>}
+        {loggedIn && <NavLink to={`users/${currentUser.id}/appointments`}>My appointments</NavLink>}
         <NavLink to="/" onClick={() => dispatch(logOut())}>
           logout
         </NavLink>
