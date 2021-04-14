dea456257308d7aa71d78abfda871b714d60e60f
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-14 21:51:32 +0100

    update delete action in delete modal

--- src/components/DeleteModal.js
+++ src/components/DeleteModal.js
@@ -2,11 +2,13 @@ import { Fragment, useRef } from 'react';
 import { Dialog, Transition } from '@headlessui/react';
 import { TrashIcon } from '@heroicons/react/outline';
 import PropTypes from 'prop-types';
+import { useDispatch } from 'react-redux';
 
 export default function DeleteModal({
   open, setOpen, deleteObj, id,
 }) {
   const cancelButtonRef = useRef();
+  const dispatch = useDispatch();
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -77,7 +79,7 @@ export default function DeleteModal({
                 <button
                   type="button"
                   className="w-full inline-flex justify-center rounded-md border border-transparent shadow-sm px-4 py-2 bg-red-600 text-base font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 sm:ml-3 sm:w-auto sm:text-sm"
-                  onClick={() => deleteObj(id)}
+                  onClick={() => dispatch(deleteObj(id))}
                 >
                   Confirm
                 </button>
