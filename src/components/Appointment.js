dc39809abd193a52f143b81e4552ca5f9675d98e
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-14 17:37:33 +0100

    add a back button to appointment list page

--- src/components/Appointment.js
+++ src/components/Appointment.js
@@ -1,15 +1,22 @@
 import PropTypes from 'prop-types';
+import history from '../history';
 import Button from './Button';
 
 const Appointment = ({ appointment }) => (
   <div>
-    <img src={appointment.car.pictures[0]?.url} alt={appointment.name} width="250" height="200" />
+    <img
+      src={appointment.car.pictures[0]?.url}
+      alt={appointment.name}
+      width="250"
+      height="200"
+    />
     <h1>{appointment.city.toUpperCase()}</h1>
     <h2>{appointment.appointment_date}</h2>
     <div>
       <Button name="UPDATEBTN" action={() => alert('Update car')} />
       <Button name="DELETEBTN" action={() => alert('Delete appointment')} />
     </div>
+    <Button name="BACK" action={() => history.goBack()} />
   </div>
 );
 
