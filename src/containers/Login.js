e2c3b697ca6cdabe108da46c8a1ec3bcc64def3f
ericmbouwe <ericmbouwe@yahoo.fr>
2021-04-24 20:17:21 +0100

    add styles to login and sign up components

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -13,31 +13,31 @@ const Login = ({ dispatch }) => {
   };
 
   return (
-    <div>
+    <div className="container mx-auto">
       <h2 className="mt-5">SIGN IN</h2>
 
-      <form onSubmit={handleSubmit}>
-        <div className="form-group mt-3">
-          <label htmlFor="name">Email</label>
+      <form onSubmit={handleSubmit} className="text-left p-8 border shadow-sm md:max-w-md mx-auto my-4 rounded-md">
+        <div className="form-group mt-3 flex flex-col">
+          <label htmlFor="name" className="">Email</label>
           <input
             type="text"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="ml-3 p-1 border border-gray-300 text-gray-500"
+            className="p-1 border border-gray-300 text-gray-500 focus:ring-green-200"
           />
         </div>
-        <div className="form-group my-3">
+        <div className="form-group my-3 flex flex-col">
           <label htmlFor="name">Password</label>
           <input
-            type="text"
+            type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="ml-3 p-1 border border-gray-300 text-gray-500"
+            className="p-1 border border-gray-300 text-gray-500 focus:ring-green-200"
           />
         </div>
-        <button type="submit">login</button>
+        <button type="submit" className="w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-green-100 text-base font-medium text-gray-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:w-auto sm:text-sm">login</button>
       </form>
     </div>
   );
--- src/containers/Registration.js
+++ src/containers/Registration.js
@@ -14,41 +14,50 @@ const Registration = ({ dispatch }) => {
   };
 
   return (
-    <div>
+    <div className="container mx-auto">
       <h2 className="mt-5">SIGN UP</h2>
-
-      <form onSubmit={handleSubmit}>
-        <div className="form-group mt-3">
+      <form
+        onSubmit={handleSubmit}
+        className="text-left p-8 border shadow-sm md:max-w-md mx-auto my-4 rounded-md"
+      >
+        <div className="form-group mt-3 flex flex-col">
           <label htmlFor="name">Name</label>
           <input
             type="text"
             id="name"
             value={name}
             onChange={(e) => setName(e.target.value)}
-            className="ml-3 p-1 border border-gray-300 text-gray-500"
+            className="p-1 border border-gray-300 text-gray-500 focus:ring-green-200"
           />
         </div>
-        <div className="form-group mt-3">
-          <label htmlFor="name">Email</label>
+        <div className="form-group mt-3 flex flex-col">
+          <label htmlFor="name" className="">
+            Email
+          </label>
           <input
             type="text"
             id="email"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
-            className="ml-3 p-1 border border-gray-300 text-gray-500"
+            className="p-1 border border-gray-300 text-gray-500 focus:ring-green-200"
           />
         </div>
-        <div className="form-group my-3">
+        <div className="form-group my-3 flex flex-col">
           <label htmlFor="name">Password</label>
           <input
-            type="text"
+            type="password"
             id="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
-            className="ml-3 p-1 border border-gray-300 text-gray-500"
+            className="p-1 border border-gray-300 text-gray-500 focus:ring-green-200"
           />
         </div>
-        <button type="submit">Register</button>
+        <button
+          type="submit"
+          className="w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-green-100 text-base font-medium text-gray-700 hover:bg-green-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 sm:mt-0 sm:w-auto sm:text-sm"
+        >
+          login
+        </button>
       </form>
     </div>
   );
