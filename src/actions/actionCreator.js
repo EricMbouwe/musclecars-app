bc090fd8b9d255c8c47071295273767a3e931d98
ericmbouwe <ericmbouwe@yahoo.fr>
2021-05-01 09:28:42 +0100

    add axios default headers to actions

--- src/actions/actionCreator.js
+++ src/actions/actionCreator.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import * as actions from './actionTypes';
 import history from '../history';
 
-// axios.defaults.baseURL = 'https://musclecarsapi.herokuapp.com';
-axios.defaults.baseURL = 'http://localhost:4000';
+axios.defaults.baseURL = 'https://musclecarsapi.herokuapp.com';
+// axios.defaults.baseURL = 'http://localhost:4000';
 
 // REQUEST ACTIONS
 const requestingData = () => ({ type: actions.REQUESTING_DATA });
